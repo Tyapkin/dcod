5f522b6563982605b59565ff089b3a643d30a2c2
Alexander Tyapkin <atyapkin20@gmail.com>
2016-10-07 14:14:59 +0300

    index view values display

--- structures/views.py
+++ structures/views.py
@@ -4,9 +4,11 @@ from django.core.urlresolvers import reverse
 
 from .forms import UploadJsonForm
 from .utils import handle_uploaded_file
+from .models import Structure
 
 def index(request):
-    return render(request, 'structures/index.html', {})
+    qs = Structure.objects.all()
+    return render(request, 'structures/index.html', {'objects': qs})
 
 def form_upload(request):
     form = UploadJsonForm(request.POST, request.FILES or None)
